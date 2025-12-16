f4f599c4e23efc877dd0672f89696818ee9c9528
Tobias Gassmann <tobias@gaszmann.de>
2025-12-16 17:04:01 +0100

    Update Imprint.vue with contact info and GitHub link
    Added imprint information and GitHub link to the Imprint page.

--- src/views/Imprint.vue
+++ src/views/Imprint.vue
@@ -1,14 +1,47 @@
 <template>
-  <pre>
+  <div class="imprint">
+    <pre>
         tobiga UG (haftungsbeschränkt)
         Tobias Gassmann
         Bodenseestr. 4a
         81241 München
         HRB 219431
         USt-IdNr.: DE 301206623
-  </pre>
+    </pre>
+
+    <a
+      class="github-link"
+      href="https://github.com/tobias74/motion-photo-gallery"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      https://github.com/tobias74/motion-photo-gallery
+    </a>
+  </div>
 </template>
-  
+
 <script>
 </script>
+
+<style scoped>
+.imprint pre {
+  margin: 0;
+  white-space: pre; /* Einrückung bleibt exakt wie im Template */
+}
+
+.github-link {
+  display: inline-block;
+  margin-top: 0.75rem;
+
+  font-family: system-ui, -apple-system, "Segoe UI", Roboto, Arial, sans-serif;
+  font-size: 0.95rem;
+  color: #555;
+  text-decoration: none;
+}
+
+.github-link:hover {
+  color: #000;
+  text-decoration: underline;
+}
+</style>
   
